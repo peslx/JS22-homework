543c73f42317ea3aa535ca6b250bc3cd6aadc511
peslx <90468331+peslx@users.noreply.github.com>
2021-11-26 21:10:48 +0900

    Выполнено задание второго урока

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+"Use strict";
+
 let title, screens, screenPrice, rollback, fullPrice, adaptive;
 
-alert("В работе");
+// 1
+title = "JS22-homework";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 1200;
+rollback = 20;
+fullPrice = 10000;
+adaptive = true;
 
-console.log("В работе");
+// 2
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов - ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта - ${fullPrice} рублей`);
+console.log(fullPrice * (rollback / 100));
