d0ed3dc01a6779eba1de755e9fc8cfddc319f433
peslx <90468331+peslx@users.noreply.github.com>
2021-12-01 00:22:34 +0300

    Выполнено задание 3 урока

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const validPromptNumber = (text) => {
     console.log(CANCEL);
     return false;
   }
-  while (isNaN(t) || +t <= 0 || !parseInt(t)) {
+  while (isNaN(+t) || +t <= 0 || !parseInt(+t)) {
     t = prompt(
       "ОШИБКА\nВведено некорректное значение. Можно указать только целое, положительно число."
     );
@@ -26,7 +26,7 @@ const validPromptNumber = (text) => {
       break;
     }
   }
-  console.log(t === null ? "" : +t);
+  // console.log(t === null ? "" : +t);
   return t === null ? false : +t;
 };
 
@@ -58,55 +58,81 @@ let screenPrice = screens
   : false;
 
 //4 Адапитив
-// let screens = validPrompt("Нужен ли адаптив на сайте? (Да/Нет)");
-// while (
-//   screens.toLowerCase() !== "да" &&
-//   screens.toLowerCase() !== "нет" &&
-//   screens !== CANCEL
-// ) {
-//   screens = validPrompt(
-//     "Можно ввести только 'да' или 'нет' (Регистр не важен)"
-//   ).toLowerCase();
-//   if (screens === CANCEL) break;
-// }
-// screens = screens === "да" ? true : false;
-// console.log(screens);
+let adaptive = screenPrice
+  ? validPrompt("Нужен ли адаптив на сайте? (Да/Нет)")
+  : false;
+
+if (adaptive) {
+  while (
+    adaptive.toLowerCase() !== "да" &&
+    adaptive.toLowerCase() !== "нет" &&
+    adaptive !== CANCEL
+  ) {
+    adaptive = validPrompt(
+      "Можно ввести только 'да' или 'нет' (Регистр не важен)"
+    ).toLowerCase();
+    if (adaptive === CANCEL) break;
+  }
+  adaptive = adaptive === "да" ? true : false;
+  // console.log(adaptive);
+}
 
 //5 Доп. услуги
 
-// let addOption1 = screenPrice
-//   ? validPrompt("Если нужен дополнительный тип услуги, то укажите какой:")
-//   : false;
+let addOption1 = screenPrice
+  ? validPrompt("Если нужен дополнительный тип услуги, то укажите какой:")
+  : false;
 
-// let addOptPrice1 = addOption1
-//   ? validPromptNumber("Сколько это будет стоить?")
-//   : false;
+let addOptPrice1 = addOption1
+  ? validPromptNumber("Сколько это будет стоить?")
+  : false;
 
-// let addOption2 = addOptPrice1
-//   ? validPrompt(
-//       "Возможно, Вы хотите добавить еще один тип услуги?\nЕсли да, то укажите какой:"
-//     )
-//   : false;
+let addOption2 = addOptPrice1
+  ? validPrompt(
+      "Возможно, Вы хотите добавить еще один тип услуги?\nЕсли да, то укажите какой:"
+    )
+  : false;
 
-// let addOptPrice2 = addOption2
-//   ? validPromptNumber("Сколько это будет стоить?")
-//   : false;
-// console.log("addOption1 = " + addOption1);
-// console.log("addOptPrice1 = " + addOptPrice1);
-// console.log("addOption2 = " + addOption2);
-// console.log("addOptPrice2 = " + addOptPrice2);
+let addOptPrice2 = addOption2
+  ? validPromptNumber("Сколько это будет стоить?")
+  : false;
 
 //6 Итоговая стоимость
-// let fullPrice = screenPrice ? screenPrice + addOption1 + addOption2 : false;
-// console.log("fullPrice = " + fullPrice);
+let fullPrice = screenPrice ? screenPrice + addOptPrice1 + addOptPrice2 : 0;
+let rollback = fullPrice * 0.2;
+let servicePercentPrice = Math.ceil(fullPrice - rollback);
 
-// let rollback = 20;
-// let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+// Вывод переменных и типов данных в них
+console.log(`title = ${title} (${typeof title})`);
+console.log(`screens = ${screens} (${typeof screens})`);
+console.log(`screenPrice = ${screenPrice} (${typeof screenPrice})`);
+console.log(`adaptive = ${adaptive} (${typeof adaptive})`);
+console.log(`addOption1 = ${addOption1} (${typeof addOption1})`);
+console.log(`addOptPrice1 = ${addOptPrice1} (${typeof addOptPrice1})`);
+console.log(`addOption2 = ${addOption2} (${typeof addOption2})`);
+console.log(`addOptPrice2 = ${addOptPrice2} (${typeof addOptPrice2})`);
+console.log(`fullPrice = ${fullPrice} (${typeof fullPrice})`);
+console.log(
+  `servicePercentPrice = ${servicePercentPrice} (${typeof servicePercentPrice})`
+);
 
-// console.log(title);
-// console.log(adaptive);
-// console.log(screens);
+// Скидка
+switch (true) {
+  case fullPrice > 30000:
+    console.log("Даем скидку в 10%");
+    break;
 
-// console.log(`Стоимость верстки экранов - ${screenPrice} рублей`);
-// console.log(`Стоимость разработки сайта - ${fullPrice} рублей`);
-// console.log(servicePercentPrice);
+  case fullPrice >= 15000 && fullPrice <= 30000:
+    console.log("Даем скидку в 5%");
+    break;
+
+  case fullPrice >= 0 && fullPrice < 15000:
+    console.log("Скидка не предусмотрена");
+    break;
+
+  case fullPrice < 0:
+    console.log("Что то пошло не так");
+    break;
+}
+
+// ₽
