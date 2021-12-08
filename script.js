f7d5967a7f2a34d997b3efedb6230b04cdea88f5
peslx <90468331+peslx@users.noreply.github.com>
2021-12-08 23:13:48 +0300

    Выполнена основная часть 8 урока

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const app = {
     adaptive: "",
     rollback: 15,
     screenPrice: 0,
+    services: {},
     allServicePrices: 0,
     fullPrice: 0,
     servicePercentPrices: 0,
@@ -20,7 +21,7 @@ const app = {
       title = title.toLowerCase();
       title = title[0].toUpperCase() + title.substring(1);
     }
-    return title;
+    app.data.title = title;
   },
 
   // Типы экранов (string)
@@ -29,12 +30,12 @@ const app = {
       "Какие типы экранов нужно разработать",
       "Простые, Сложные, Интерактивные"
     );
-    return screens.split(", ");
+    app.data.screens = screens.split(", ");
   },
 
   // Адапитив (boolean)
   isAdaptive: () => {
-    return confirm("Нужен ли адаптив на сайте?");
+    app.data.adaptive = confirm("Нужен ли адаптив на сайте?");
   },
 
   // Проверка на число (boolean)
@@ -53,95 +54,71 @@ const app = {
         "    100000n  "
       );
     } while (!app.isNum(screenPrice) && screenPrice !== null);
-    return +screenPrice;
+    app.data.screenPrice = +screenPrice;
   },
 
   // Доп. услуги (number)
   getAllServicePrices: () => {
-    let addOption1 = prompt(
-      "Если нужен дополнительный тип услуги, то укажите какой:",
-      "Админ-панель"
-    );
+    for (let i = 0; i < 2; i++) {
+      let name = prompt(
+        "Если нужен дополнительный тип услуги, то укажите какой:",
+        "Админ-панель/База данных"
+      );
+      let price = 0;
 
-    let addOptPrice1;
-    if (addOption1) {
       do {
-        addOptPrice1 = prompt("Сколько это будет стоить?", 5000);
-      } while (!app.isNum(addOptPrice1) && addOptPrice1 !== null);
-
-      if (addOptPrice1 === null) {
-        addOptPrice1 = 0;
-      } else {
-        addOptPrice1 = isNaN(+addOptPrice1) ? 0 : +addOptPrice1;
-      }
+        price = prompt("Сколько это будет стоить?", 5000);
+      } while (!app.isNum(price) && price !== null);
+      app.data.services[`${i + 1}_${name}`] = +price;
     }
 
-    let addOption2 = addOptPrice1
-      ? prompt(
-          "Возможно, Вы хотите добавить еще один тип услуги?\nЕсли да, то укажите какой:",
-          "База данных"
-        )
-      : false;
-
-    let addOptPrice2;
-    if (addOption2 && addOption2 !== null) {
-      do {
-        addOptPrice2 = prompt("Сколько это будет стоить?", 3500);
-      } while (!app.isNum(addOptPrice2) && addOptPrice2 !== null);
-
-      if (addOptPrice2 === null) {
-        addOptPrice2 = 0;
-      } else {
-        addOptPrice2 = isNaN(+addOptPrice2) ? 0 : +addOptPrice2;
-      }
-    } else {
-      addOptPrice2 = 0;
+    for (const key in app.data.services) {
+      app.data.allServicePrices += app.data.services[key];
     }
-
-    return addOptPrice1 ? +addOptPrice1 + addOptPrice2 : 0;
+    console.log(JSON.stringify(app.data.services));
   },
 
   // Итоговая стоимость (number)
-  getFullPrice: () => app.data.screenPrice + app.data.allServicePrices,
+  getFullPrice: () =>
+    (app.data.fullPrice = app.data.screenPrice + app.data.allServicePrices),
 
   // Cтоимость за вычетом отката посреднику (number)
   getServicePercentPrices: (fullPrice, percent) =>
-    Math.ceil(fullPrice - (fullPrice * percent) / 100),
+    (app.data.servicePercentPrices = Math.ceil(
+      fullPrice - (fullPrice * percent) / 100
+    )),
 
   // Скидка (string)
   getDiscountInfo: (fullPrice) => {
     switch (true) {
       case fullPrice > 30000:
-        return "Даем скидку в 10%";
+        app.data.discountInfo = "Даем скидку в 10%";
         break;
 
       case fullPrice >= 15000 && fullPrice <= 30000:
-        return "Даем скидку в 5%";
+        app.data.discountInfo = "Даем скидку в 5%";
         break;
 
       case fullPrice >= 0 && fullPrice < 15000:
-        return "Скидка не предусмотрена";
+        app.data.discountInfo = "Скидка не предусмотрена";
         break;
 
       case fullPrice < 0:
-        return "Что то пошло не так";
+        app.data.discountInfo = "Что то пошло не так";
         break;
     }
   },
 
   // Старт ввода данных
   startQuiz: () => {
-    app.data.title = app.getTitle();
-    app.data.screens = app.getScreens();
-    app.data.adaptive = app.isAdaptive();
-    app.data.screenPrice = app.getScreenPrice();
-    app.data.allServicePrices = app.getAllServicePrices();
-    app.data.fullPrice = app.getFullPrice();
-    app.data.servicePercentPrices = app.getServicePercentPrices(
-      app.data.fullPrice,
-      app.data.rollback
-    );
-    app.data.discountInfo = app.getDiscountInfo(app.data.fullPrice);
+    app.getTitle();
+    app.getScreens();
+    app.isAdaptive();
+    app.getScreenPrice();
+    app.getAllServicePrices();
+    app.getFullPrice();
+    app.getServicePercentPrices(app.data.fullPrice, app.data.rollback);
+    app.getDiscountInfo(app.data.fullPrice);
   },
 
   logger: (obj) => {
