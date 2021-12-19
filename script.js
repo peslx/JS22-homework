a209a2c6479f741c90acc1dd9f93efe1ecf4759f
peslx <90468331+peslx@users.noreply.github.com>
2021-12-19 22:43:49 +0900

    Добавлена валидация

--- script.js
+++ script.js
@@ -2,17 +2,37 @@
 
 const todoForm = document.querySelector(".todo-control");
 const todoInput = document.querySelector(".header-input");
-
 const todo = document.getElementById("todo");
 const completed = document.getElementById("completed");
 
-const todoData = [];
+let todoData;
+
+// Загрузка данных из localStorage (первый запуск)
+const loadData = () => {
+  if (Boolean(localStorage.getItem("todoData"))) {
+    todoData = JSON.parse(localStorage.getItem("todoData"));
+  } else {
+    todoData = [];
+    localStorage.setItem("todoData", JSON.stringify(todoData));
+  }
+};
+
+// Сохранение данных в localStorage
+const saveData = () => {
+  localStorage.setItem("todoData", JSON.stringify(todoData));
+};
+
+// Сбор данных из localStorage
+const readData = () => {
+  todoData = JSON.parse(localStorage.getItem("todoData"));
+};
 
 // Отрисовка базы данных списка дел
 const renderData = () => {
+  saveData();
+  readData();
   todo.innerHTML = "";
   completed.innerHTML = "";
-  console.log(todoData);
   todoData.forEach((item, index) => {
     // console.log(item);
     const li = document.createElement("li");
@@ -25,10 +45,15 @@ const renderData = () => {
 	</div>`;
 
     item.done ? completed.append(li) : todo.append(li);
+
+    // Действия по кнопкам
+    // Изменение статуса
     li.querySelector(".todo-complete").addEventListener("click", () => {
       item.done = !item.done;
       renderData();
     });
+
+    // Удаление
     li.querySelector(".todo-remove").addEventListener("click", () => {
       todoData.splice(index, 1);
       renderData();
@@ -36,14 +61,29 @@ const renderData = () => {
   });
 };
 
+loadData();
+renderData();
+
 // Добавление элемента в список дел
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const newTodo = {
-    text: todoInput.value,
-    done: false,
-  };
-  todoData.push(newTodo);
-  todoInput.value = "";
-  renderData();
+  // проверить на пустую строку
+  // todoInput.value
+  let t;
+
+  t = todoInput.value;
+  t = todoInput.value.trim();
+
+  if (t === "") {
+    todoInput.value = "";
+    alert("Нельзя вводить пустое значение!\nПовторите ввод");
+  } else {
+    const newTodo = {
+      text: t,
+      done: false,
+    };
+    todoData.push(newTodo);
+    todoInput.value = "";
+    renderData();
+  }
 });
