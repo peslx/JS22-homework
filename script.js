530af2c662f64d66651e7290a9b857c36b02e142
peslx <90468331+peslx@users.noreply.github.com>
2022-01-06 20:06:03 +0900

    Выполено задание 26 урока

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const getData = (getUrl) => {
+  return fetch(getUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Получено из 'getData()': ", data);
+      return data;
+    })
+    .catch((error) => console.log("Ошибка выполнения запроса => " + error));
+};
+
+const sendData = (data, url) => {
+  console.log("Отправлено из 'sendData()' на сервер: ", data);
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("Получено от сервера: ", result);
+    })
+    .catch((error) => console.log("Ошибка выполнения запроса => " + error));
+};
+
+// "./db.json"
+// "https://jsonplaceholder.typicode.com/posts"
+
+getData("./db.json").then((data) =>
+  sendData(data, "https://jsonplaceholder.typicode.com/posts")
+);
